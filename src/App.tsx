bb5b3da4674363dd8b0da3d39460e9178e84c4d3
Eugen Luchianov <eugenluchianov97@gmail.com>
2023-09-03 20:45:15 +0300

    03.09.2023 20:45

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,14 @@ import Calendar from "./components/Calendar/Component"
 function App() {
 
     const weekDays:any = {
-        0:"ВС",
+
         1:"ПН",
         2:"ВТ",
         3:"СР",
         4:"ЧТ",
         5:"ПТ",
         6:"СБ",
+        0:"ВС",
     }
     const today = new Date();
     const [currentMonth, setCurrentMonth] = useState(today.getMonth())
@@ -35,14 +36,16 @@ function App() {
     }
 
     const getDaysInMonth = (year:number, month:number) => {
-        return new Date(year, month, 0).getDate();
+        return new Date(year, month+1, 0).getDate();
     }
 
 
     // const currentDay = today.getDay()
     const currentDay = today.getDate()
 
-    const firstDayOfNextMonth = new Date(currentYear, currentMonth, 1).getDay();
+    const lastDayOfPrevMonth = new Date(currentYear, currentMonth, 0).getDay(); //трицательные значения берут прошлый месяц
+    const firstDayOfThisMonth = new Date(currentYear, currentMonth, 1).getDay(); //трицательные значения берут прошлый месяц
+    const firstDayOfNextMonth = new Date(currentYear, currentMonth+1, 1).getDay();
 
     const nextMonth = () => {
         if(currentMonth + 1 > 11){
@@ -63,6 +66,37 @@ function App() {
             setCurrentMonth(currentMonth - 1)
         }
     }
+    //первый день прошлого месяца который был пн (индекс 1)
+
+    const getLastDays = (lastDayOfPrevMonth:number) => {
+        let getLastDays:any = [];
+        //если первый день недели это пн , то нам не нужны предыдущие дни
+        if(lastDayOfPrevMonth > 0){
+            for(let i = 1;i<=lastDayOfPrevMonth;i++){
+                getLastDays.push(weekDays[i])
+            }
+        }
+
+        return getLastDays;
+    }
+
+    const getNextDays = (firstDayOfNextMonth:number) => {
+        console.log(firstDayOfNextMonth + '-------------------')
+        let getLastDays:any = [];
+        //если первый день недели это пн (1) , то нам не нужны следующие дни
+        if(firstDayOfNextMonth > 1 || firstDayOfNextMonth === 0 ){
+            for(let i = 1;i<=firstDayOfNextMonth;i++){
+                getLastDays.push(weekDays[i])
+            }
+        }
+
+        return getLastDays;
+    }
+
+    console.log(getLastDays(lastDayOfPrevMonth))
+    console.log(getNextDays(firstDayOfNextMonth))
+    ;
+
     return (
     <div>
         Первый день месяца - {weekDays[firstDayOfNextMonth]}<br/>
@@ -70,11 +104,15 @@ function App() {
         {/*{currentDay} {month[currentMonth]} {currentYear}*/}
         {currentDay}/ {month[currentMonth]}/ {currentYear}
 
+        <br/>Количество дней в месяце
+        {getDaysInMonth(currentYear,currentMonth)}
+
 
         <br/>
         <button onClick={prevMonth}>Prev</button>
         <button onClick={nextMonth}>Next</button>
 
+        {lastDayOfPrevMonth}
 
 
 
