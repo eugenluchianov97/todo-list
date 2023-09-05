0be04e7d04a49bd95932597decf4c4175e89a6ed
Eugen Customagic <tisagiy415@soremap.com>
2023-09-05 16:49:42 +0300

    05.09.2023 16:49

--- src/App.tsx
+++ src/App.tsx
@@ -15,11 +15,18 @@ function App() {
         5:"ПТ",
         6:"СБ",
         7:"ВС",
-
     }
-    const today = new Date();
-    const [currentMonth, setCurrentMonth] = useState(today.getMonth())
-    const [currentYear, setCurrentYear] = useState(today.getFullYear())
+
+    const weekDaysFull:any = {
+
+        1:"Понедельник",
+        2:"Вторник",
+        3:"Среда",
+        4:"Четверг",
+        5:"Пятница",
+        6:"Суббота",
+        7:"Воскресенье",
+    }
 
     const month:any = {
         0:"Январь",
@@ -36,22 +43,82 @@ function App() {
         11:"Декабрь",
     }
 
+    const today = new Date();
+    const [currentMonth, setCurrentMonth] = useState(today.getMonth())
+    const [currentYear, setCurrentYear] = useState(today.getFullYear())
+
     const getDaysInMonth = (year:number, month:number) => {
         return new Date(year, month+1, 0).getDate();
     }
 
-
-    // const currentDay = today.getDay()
     const currentDay = today.getDate()
+    const dayOfWeek = (day:number) => {
+        return (day === 0) ? 7 : day
+    }
 
-    const lastDayOfPrevMonth = new Date(currentYear, currentMonth, 0).getDay(); //трицательные значения берут прошлый месяц
-    const firstDayOfThisMonth = new Date(currentYear, currentMonth, 1).getDay(); //трицательные значения берут прошлый месяц
-    const lastDayOfThisMonth = new Date(currentYear, currentMonth, getDaysInMonth(currentYear, currentMonth)).getDay(); //трицательные значения берут прошлый месяц
-    const firstDayOfNextMonth = new Date(currentYear, currentMonth, getDaysInMonth(currentYear, currentMonth) + 1).getDay();
+    const renderList = () => {
+        let days:any[] = [];
+        let lastDayLastMonth:any = dayOfWeek(new Date(currentYear, currentMonth, 0).getDay());
+        if(lastDayLastMonth != 7){
+            for(let i = lastDayLastMonth;i > 0;i--){
+                let date:number = new Date(currentYear, currentMonth, 1 - i ).getDate()
+                let day:number = new Date(currentYear, currentMonth, 1 - i ).getDay()
+                days.push({
+                    date:date,
+                    day:dayOfWeek(day),
+                    active:false,
+                    year:currentYear,
+                    month:currentMonth-1,
+                    today:false,
+                    past:currentYear < today.getFullYear() ||
+                        (currentYear === today.getFullYear() && currentMonth < today.getMonth() ) ||
+                        (currentYear === today.getFullYear() && currentMonth === today.getMonth() &&  i < today.getDate())
+                })
+            }
+        }
 
+        for(let i:number = 1;i <= getDaysInMonth(currentYear,currentMonth);i++){
 
+            let day:number = new Date(currentYear, currentMonth, i).getDay()
+            //если год меньше
+            days.push({
+                date:i,
+                day:dayOfWeek(day),
+                active:true,
+                year:currentYear,
+                month:currentMonth,
+                today:i === today.getDate() && currentMonth === today.getMonth() && currentYear === today.getFullYear(),
+                past:currentYear < today.getFullYear() ||
+                    (currentYear === today.getFullYear() && currentMonth < today.getMonth() ) ||
+                    (currentYear === today.getFullYear() && currentMonth === today.getMonth() &&  i < today.getDate())
+            })
+        }
 
-    //если текущий  первый день это среда
+        let daysCount:number = getDaysInMonth(currentYear,currentMonth)
+        let firstDayNextMonth:number = dayOfWeek(new Date(currentYear, currentMonth, daysCount + 1).getDay());
+
+        if(firstDayNextMonth != 1){
+
+            for(let i:number = firstDayNextMonth, d:number = 1;i <= 7;i++,d++){
+                let date:number = new Date(currentYear, currentMonth, daysCount + d ).getDate()
+                let day:number = new Date(currentYear, currentMonth, daysCount + d ).getDay()
+                days.push({
+                    date:date,
+                    day:dayOfWeek(day),
+                    active:false,
+                    month:currentMonth+1,
+                    year:currentYear,
+                    today:false,
+                    past:currentYear < today.getFullYear() ||
+                        (currentYear === today.getFullYear() && currentMonth < today.getMonth() ) ||
+                        (currentYear === today.getFullYear() && currentMonth === today.getMonth() &&  i < today.getDate())
+                })
+            }
+        }
+
+        console.log(days)
+        return days;
+    }
 
     const nextMonth = () => {
         if(currentMonth + 1 > 11){
@@ -72,121 +139,73 @@ function App() {
             setCurrentMonth(currentMonth - 1)
         }
     }
-    //первый день прошлого месяца который был пн (индекс 1)
 
-    const getLastDays = (lastDayOfPrevMonth:number) => {
-        let getLastDays:any = [];
-        //если первый день недели это пн , то нам не нужны предыдущие дни
-        if(lastDayOfPrevMonth > 0){
-            for(let i = 1;i<=lastDayOfPrevMonth;i++){
-                getLastDays.push(weekDays[i])
-            }
-        }
 
-        return getLastDays;
+    const handleClick = (day:any) => {
+           console.log(day);
     }
-
-    const getNextDays = (firstDayOfNextMonth:number) => {
-
-        let getLastDays:any = [];
-        //если первый день недели это пн (1) , то нам не нужны следующие дни
-        if(firstDayOfNextMonth === 1 ){
-            for(let i = 1;i<=firstDayOfNextMonth;i++){
-                getLastDays.push(weekDays[i])
-            }
-        }
-
-        return getLastDays;
-    }
-
-    console.log(getLastDays(lastDayOfPrevMonth))
-    console.log(getNextDays(firstDayOfNextMonth))
-
-
-
-
-
-    const test = () => {
-        return (
-            <div>
-                { Object.entries(weekDays).map((day:any) => {
-                    console.log(day[0])
-                    return (
-                        <div>{day[1]}</div>
-                    )
-                })}
-            </div>
-        )
-    }
-
-    const dayOfWeek = (day:number) => {
-        return (day === 0) ? 7 : day
-    }
-
-    const render = () => {
-        let days = [];
-       let lastDayLastMonth = dayOfWeek(new Date(currentYear, currentMonth, 0).getDay());
-       if(lastDayLastMonth != 7){
-           for(let i = lastDayLastMonth;i > 0;i--){
-               let date = new Date(currentYear, currentMonth, 1 - i ).getDate()
-               let day = new Date(currentYear, currentMonth, 1 - i ).getDay()
-               days.push({date:date,day:dayOfWeek(day),active:false})
-           }
-       }
-
-        for(let i = 1;i <= getDaysInMonth(currentYear,currentMonth);i++){
-
-            let day = new Date(currentYear, currentMonth, i).getDay()
-            days.push({date:i,day:dayOfWeek(day),active:true})
-        }
-
-        let daysCount = getDaysInMonth(currentYear,currentMonth)
-        let firstDayNextMonth = dayOfWeek(new Date(currentYear, currentMonth, daysCount + 1).getDay());
-
-        console.log(firstDayNextMonth)
-        if(firstDayNextMonth != 1){
-            //цикл с первого дня до вс
-            for(let i = firstDayNextMonth, d = 1;i <= 7;i++,d++){
-                let date = new Date(currentYear, currentMonth, daysCount + d ).getDate()
-                let day = new Date(currentYear, currentMonth, daysCount + d ).getDay()
-                days.push({date:date,day:dayOfWeek(day),active:false})
-            }
-        }
-
-        console.log(days)
-    }
-    render()
 
 
 
     return (
-    <div>
-        Первый день месяца - - {firstDayOfThisMonth}<br/>
-        Количество дней в месяце - {getDaysInMonth(currentYear,currentMonth)}<br/>
-        {currentDay}/ {month[currentMonth]}/ {currentYear}
+    <div className="container flex bg-teal-300 m-auto ">
 
-        <br/>Количество дней в месяце
-        {getDaysInMonth(currentYear,currentMonth)}
+        <div className="flex flex-col items-center justify-center w-4/12 bg-teal-300 ">
+
+            <h1 className="uppercase font-light text-white m-6 select-none">{weekDaysFull[today.getDay()]}</h1>
+            <p className="font-light text-9xl text-white mb-8 select-none">{currentDay}</p>
+        </div>
+        <div className="w-8/12 bg-white">
+
+            <div className="prev-next-container flex justify-around items-center p-8">
+                <button onClick={prevMonth}>
+                    <i className="fa fa-chevron-left text-slate-300 font-bold text-4xl"></i>
+                </button>
+                <p className="text-xl text-teal-600 uppercase font-semibold">{month[currentMonth]} {currentYear}</p>
+                <button onClick={nextMonth}>
+                    <i className="fa fa-chevron-right text-slate-300 font-bold text-4xl"></i>
+                </button>
+            </div>
+
+            <div className="days-container">
+                {Object.entries(weekDays).map((day:any, idx) => {
+                    return (
+                        <div key={idx} className="day-heading select-none font-semibold text-pink-600 flex items-center justify-center m-1">{day[1]}</div>
+                    )
+                })}
+            </div>
 
 
-        <br/>
-        <button onClick={prevMonth}>Prev</button>
-        <button onClick={nextMonth}>Next</button>
+            <div className="days-container pb-8">
 
-        {lastDayOfPrevMonth}
-        <div className="container">
-            <div className="day">ПН</div>
-            <div className="day">ВТ</div>
-            <div className="day">СР</div>
-            <div className="day">ЧТ</div>
-            <div className="day">ПТ</div>
-            <div className="day">СБ</div>
-            <div className="day">ВС</div>
+                {
+                    renderList().map((day, idx) => {
+                        let disabledClass = 'text-slate-400 ';
+                        let todayClass = 'border-4 font-semibold rounded-full border-pink-600 bg-pink-200 text-pink-600  ';
+                        let pastClass = "border-4  rounded-full border-slate-100  bg-slate-100 ";
+
+
+                        let className:string = "day m-1  flex items-center justify-center cursor-pointer font-medium ";
+                        if(!day.active){
+                            className += disabledClass
+                        }
+                        if(day.past){
+                            className += pastClass
+                        }
+
+
+                        className = className + (day.today  ? todayClass : '' )
+                        return (
+
+                            <div onClick={() => {handleClick(day)}} className={className}>{day.date}</div>
+                        )
+                    })
+                }
+            </div>
         </div>
 
-        {
-            test()
-        }
+
+
 
 
     </div>
